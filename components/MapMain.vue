71cf92f682b4e3679e36967c3921d61d2a264eda
Zhigulskiy <evgeniy1st@ya.ru>
2023-09-30 21:21:59 +0300

    dev: map component complete

--- components/MapMain.vue
+++ components/MapMain.vue
@@ -3,7 +3,7 @@
     <div class="flex items-center gap-[150px] mb-[24px] px-[148px]">
       <div class="relative">
         <button
-          class="flex items-center gap-[18px] border-b border-b-basic-black"
+          class="flex items-center gap-[18px] border-b border-b-basic-black min-w-[190px] justify-between"
           @click="isDistrict = !isDistrict"
         >
           <span class="">
@@ -42,7 +42,7 @@
         <ul
           v-if="isDistrict"
           v-on-click-outside="() => (isDistrict = false)"
-          class="absolute bg-white top-[100%] py-[10px] rounded-md max-h-[300px] overflow-y-auto"
+          class="absolute bg-white top-[100%] py-[10px] rounded-md max-h-[300px] overflow-y-auto z-10"
         >
           <li v-for="item in data.data.districts" :key="item.id">
             <button
@@ -55,12 +55,30 @@
         </ul>
       </div>
 
-      <div>
+      <div class="relative">
         <button
-          class="flex items-center gap-[18px] border-b border-b-basic-black"
+          class="flex items-center gap-[18px] border-b border-b-basic-black disabled:opacity-60 min-w-[190px] justify-between"
+          :disabled="stationsList.length === 0"
+          @click="isStation = !isStation"
         >
-          <span class=""> Метро </span>
-          <span class="w-[18px] h-[14px]">
+          <span class="">
+            {{ currentStation ? currentStation : 'Метро' }}
+          </span>
+          <span v-if="!isStation" class="w-[18px] h-[14px]">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="11"
+              viewBox="0 0 16 11"
+              fill="none"
+            >
+              <path
+                d="M1.19963 0.999998L8 10.1611L14.8004 0.999999L1.19963 0.999998Z"
+                stroke="#141414"
+              />
+            </svg>
+          </span>
+          <span v-else class="w-[18px] h-[14px] rotate-180">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="16"
@@ -75,6 +93,21 @@
             </svg>
           </span>
         </button>
+
+        <ul
+          v-if="isStation"
+          v-on-click-outside="() => (isStation = false)"
+          class="absolute bg-white top-[100%] py-[10px] rounded-md max-h-[300px] overflow-y-auto z-10"
+        >
+          <li v-for="item in stationsList" :key="item.id">
+            <button
+              class="px-[15px] hover:bg-slate-100 py-[3px] w-full text-left"
+              @click="(currentStation = item.name), (isStation = false)"
+            >
+              {{ item.name }}
+            </button>
+          </li>
+        </ul>
       </div>
 
       <button
@@ -93,8 +126,24 @@
       </button>
     </div>
 
-    <div class="w-full px-[32px] h-[540px] rounded-[60px] mb-[40px]">
-      <img :src="`${api}assets/${img}`" alt="Карта" width="1376" height="540" />
+    <div
+      class="w-[1376px] px-[32px] h-[540px] rounded-[60px] mb-[40px] relative p-[30px] flex items-center justify-end mx-auto overflow-hidden"
+    >
+      <img
+        :src="`${api}assets/${img}?format=auto`"
+        alt="Карта"
+        width="1376"
+        height="540"
+        class="absolute top-0 left-0 mx-auto"
+      />
+
+      <Transition name="slide">
+        <MapMastersList
+          v-if="currentDistrict"
+          :current-station="currentStation"
+          :current-district="currentDistrict"
+        />
+      </Transition>
     </div>
 
     <div
@@ -103,6 +152,7 @@
       <div class="w-max-[383px] leading-[18px]">
         <h5 class="mb-[22px] font-[700]">Районы Санкт-Петербурга</h5>
         <ul class="flex flex-wrap">
+          <!-- @vue-ignore -->
           <li
             v-for="item in data.data.districts.filter((d) => !d.isSuburb)"
             :key="item.id"
@@ -131,6 +181,7 @@
           Пригороды Санкт-Петербурга (Ленинградская область)
         </h5>
         <ul class="flex flex-wrap gap-x-[34px]">
+          <!-- @vue-ignore -->
           <li
             v-for="item in data.data.districts.filter((d) => d.isSuburb)"
             :key="item.id"
@@ -154,16 +205,12 @@
         </ul>
       </div>
     </div>
-
-    <!-- <pre>
-      {{ data }}
-    </pre> -->
   </div>
 </template>
 
 <script setup lang="ts">
 import { vOnClickOutside } from '@vueuse/components';
-import { ref } from 'vue';
+import { ref, computed, watch } from 'vue';
 import { useFetch } from 'nuxt/app';
 const runtimeConfig = useRuntimeConfig();
 
@@ -179,7 +226,7 @@ const props = defineProps({
 const { api } = runtimeConfig.public;
 
 const { data: data }: any = useFetch(
-  `${api}items/cities/${appData.value.data.city.id}?fields=districts.name,districts.id,districts.isSuburb,districts.stations,districts.stations.name,districts.stations.id,districts.masters.masters_id.skill,districts.masters.masters_id.first_name,districts.masters.masters_id.last_name,districts.masters.masters_id.img`
+  `${api}items/cities/${appData.value.data.city.id}?fields=districts.name,districts.id,districts.isSuburb,districts.stations,districts.stations.name,districts.stations.id`
 );
 
 const isDistrict = ref(false);
@@ -187,4 +234,41 @@ const currentDistrict = ref('');
 
 const isStation = ref(false);
 const currentStation = ref('');
+
+const stationsList = computed(() => {
+  let district = data.value.data.districts.find(
+    // @ts-ignore
+    (d) => currentDistrict.value === d.name
+  );
+  if (!district) {
+    return [];
+  }
+  return district.stations;
+});
+
+watch(currentDistrict, () => {
+  currentStation.value = '';
+});
 </script>
+
+<style scoped>
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 0.5s ease-out;
+}
+
+.slide-enter-to,
+.slide-leave-from {
+  transform: translateY(0);
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  transform: translateY(-1000px);
+}
+
+.slide-leave-from {
+  position: absolute;
+  left: 0;
+}
+</style>
---
+++ components/MapMastersList.vue
@@ -0,0 +1,95 @@
+<template>
+  <div
+    class="w-[280px] h-[480px] bg-[rgba(255,255,255,0.50)] rounded-[40px] relative z-10 glass overflow-y-auto p-[30px]"
+  >
+    <ul v-if="masters.length" ref="list" class="flex flex-col gap-[22px]">
+      <li
+        v-for="master in masters"
+        :key="master.id"
+        class="flex gap-[13px] items-center"
+      >
+        <img
+          :src="`${api}assets/${master.img}?format=auto&width=42&height=42`"
+          alt="Фото мастера"
+          width="42"
+          height="42"
+          class="grow-0 shrink-0 rounded-full"
+        />
+        <div class="flex flex-col gap-[2px] leading-[18px]">
+          <span class="text-[14px]"
+            >{{ master.first_name }} {{ master.last_name }}</span
+          >
+          <span class="text-[12px]">{{ master.skill }}</span>
+        </div>
+      </li>
+    </ul>
+    <div v-else>
+      Извините, в этом месте у нас пока никого нет. Но мастер может приехать из
+      соседнего района
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import autoAnimate from '@formkit/auto-animate';
+import { computed, onMounted, ref } from 'vue';
+import { useFetch } from 'nuxt/app';
+const runtimeConfig = useRuntimeConfig();
+
+const { data: appData } = useNuxtData('appData');
+
+const props = defineProps({
+  currentDistrict: {
+    type: String,
+    default: '',
+  },
+  currentStation: {
+    type: String,
+    default: '',
+  },
+});
+
+const { api } = runtimeConfig.public;
+
+const { data: data }: any = useFetch(
+  `${api}items/masters?filter[city][_eq]=${appData.value.data.city.id}&filter[status][_eq]=active&fields=id,first_name,last_name,img,skill,districts.districts_id.name,stations.stations_id.name`
+);
+
+const list = ref();
+
+const masters = computed(() => {
+  console.log(data);
+  const all =
+    data && 'value' in data && data.value && 'data' in data.value
+      ? data.value.data
+      : [];
+  const filteredByDistricts = all.filter((m: any) => {
+    return m.districts.some(
+      (i: any) => i.districts_id.name === props.currentDistrict
+    );
+  });
+
+  if (props.currentStation) {
+    return filteredByDistricts.filter((m: any) => {
+      return m.stations.some(
+        (i: any) => i.stations_id.name === props.currentStation
+      );
+    });
+  }
+
+  return filteredByDistricts;
+});
+
+onMounted(() => {
+  if (masters.value.length) {
+    autoAnimate(list.value);
+  }
+});
+</script>
+
+<style scoped>
+.glass {
+  box-shadow: 0px 16px 26px 0px #bdc6dc;
+  backdrop-filter: blur(10px);
+}
+</style>
