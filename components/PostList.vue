dc978c56e53b41ba2d819a617c83f0e135aff9ab
Zhigulskiy <evgeniy1st@ya.ru>
2023-10-01 14:30:38 +0300

    dev: posts components (on main page)

---
+++ components/PostList.vue
@@ -0,0 +1,61 @@
+<template>
+  <ul class="flex flex-col gap-[30px]">
+    <li
+      v-for="post in posts.data"
+      :key="post.id"
+      class="flex items-start gap-[20px]"
+    >
+      <img
+        :src="`${api}assets/${post.preview_image}?format=auto&width=329&height=209&fit=cover`"
+        alt="Превью статьи"
+        width="329"
+        height="209"
+        class="rounded-[60px]"
+      />
+      <div class="flex flex-col gap-[20px] items-start grow">
+        <h5 class="text-[30px] leading-[38px]">
+          {{ post.title }}
+        </h5>
+        <p class="text-[16px] leading-[normal]">
+          {{ post.preview_text }}
+        </p>
+        <div class="w-full text-end">
+          <a
+            href="#"
+            class="px-[14px] py-[4px] rounded-[15px] text-[14px] border border-basic-black"
+          >
+            Читать
+          </a>
+        </div>
+      </div>
+    </li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { useFetch } from 'nuxt/app';
+const runtimeConfig = useRuntimeConfig();
+
+const props = defineProps({
+  count: {
+    type: [String, Number],
+    default: 3,
+  },
+  offset: {
+    type: [String, Number],
+    default: 0,
+  },
+  sort: {
+    type: String,
+    default: 'newest',
+  },
+});
+
+const { api, site } = runtimeConfig.public;
+
+const { data: posts }: any = useFetch(
+  `${api}items/posts?limit=${props.count}&offset=${props.offset}&sort=${
+    props.sort === 'newest' ? '-date_created' : 'date_created'
+  }&filter[status][_eq]=published&filter[site][_eq]=${site}`
+);
+</script>
--- components/blocks/Posts.vue
+++ components/blocks/Posts.vue
@@ -1,5 +1,41 @@
 <template>
-  <div>posts</div>
+  <section
+    class="pt-[130px] px-[148px] text-basic-black flex flex-col gap-[70px]"
+  >
+    <h4 class="text-[44px] leading-[50px]">
+      {{ config.data.title }}
+    </h4>
+
+    <PostList
+      :count="config.data.post_count"
+      :offset="config.data.post_offset"
+      :sort="config.data.post_sort"
+    />
+    <div class="w-full text-end">
+      <a
+        :href="config.data.button_link"
+        class="bg-accent-orange text-white rounded-full font-700 text-[16px] px-[15px] py-[5px]"
+      >
+        {{ config.data.button_text }}
+      </a>
+    </div>
+  </section>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useFetch } from 'nuxt/app';
+const runtimeConfig = useRuntimeConfig();
+
+const props = defineProps({
+  block: {
+    type: Object,
+    default: () => {},
+  },
+});
+
+const { api } = runtimeConfig.public;
+
+const { data: config }: any = useFetch(
+  `${api}items/${props.block.collection}/${props.block.item}?fields=*.*`
+);
+</script>
